adf8add1928a7efc4683b350007e7fd51d734ff0
hossamhossny <hossamhossnyar@gmail.com>
2017-01-02 13:37:26 +0200

    fix

--- mysql_updates/trexle_payments.sql
+++ mysql_updates/trexle_payments.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `pin` (
+CREATE TABLE `trexle` (
   `id` int(11) UNSIGNED NOT NULL,
   `customer_id` int(11) NOT NULL DEFAULT '0',
   `order_id` int(11) NOT NULL DEFAULT '0',
@@ -12,14 +12,14 @@ CREATE TABLE `pin` (
   `session_id` varchar(255) NOT NULL DEFAULT ''
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-CREATE TABLE `pin_order_token` (
-  `pin_order_token` int(11) NOT NULL,
+CREATE TABLE `trexle_order_token` (
+  `trexle_order_token` int(11) NOT NULL,
   `order_id` int(11) NOT NULL,
   `token` varchar(64) NOT NULL,
   `created_date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `pin_tokens` (
+CREATE TABLE `trexle_tokens` (
   `id` int(11) NOT NULL,
   `customer_id` int(11) NOT NULL,
   `token` varchar(64) NOT NULL,
@@ -29,22 +29,22 @@ CREATE TABLE `pin_tokens` (
   `date_added` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-ALTER TABLE `pin`
+ALTER TABLE `trexle`
   ADD PRIMARY KEY (`id`);
 
-ALTER TABLE `pin_order_token`
-  ADD PRIMARY KEY (`pin_order_token`);
+ALTER TABLE `trexle_order_token`
+  ADD PRIMARY KEY (`trexle_order_token`);
 
-ALTER TABLE `pin_tokens`
+ALTER TABLE `trexle_tokens`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `token` (`token`),
   ADD KEY `customer_id` (`customer_id`);
 
-ALTER TABLE `pin`
+ALTER TABLE `trexle`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 
-ALTER TABLE `pin_order_token`
-  MODIFY `pin_order_token` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `trexle_order_token`
+  MODIFY `trexle_order_token` int(11) NOT NULL AUTO_INCREMENT;
 
-ALTER TABLE `pin_tokens` CHANGE `id` `id` INT(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `trexle_tokens` CHANGE `id` `id` INT(11) NOT NULL AUTO_INCREMENT;
 
